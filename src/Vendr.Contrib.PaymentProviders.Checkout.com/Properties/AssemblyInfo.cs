f72b88e6174c172dd8ca5c6f979a6f005f56fdd7
Bjarne Fyrstenborg <bf@mcb.dk>
2021-04-11 21:16:03 +0200

    Update copyright year

--- src/Vendr.Contrib.PaymentProviders.Checkout.com/Properties/AssemblyInfo.cs
+++ src/Vendr.Contrib.PaymentProviders.Checkout.com/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Vendr.Contrib.PaymentProviders.Checkout.com")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
